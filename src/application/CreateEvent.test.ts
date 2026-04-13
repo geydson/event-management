f6db9b854dc5648108994307bacdd187a1917c46
Geydson Santos <geydson.santos@integralmedica.com.br>
2026-04-13 15:07:00 -0300

    test: ensure repository is called with correct params in CreateEvent use case

--- src/application/CreateEvent.test.ts
+++ src/application/CreateEvent.test.ts
@@ -13,6 +13,13 @@ describe("createEvents", () => {
   //   }
   // }
 
+  const makeSut = () => {
+    const eventRepository = new EventRepositoryDrizzle(database)
+    const sut = new CreateEvent(eventRepository)
+
+    return { sut, eventRepository }
+  }
+
   let database: typeof db
   // let container: StartedPostgreSqlContainer
 
@@ -33,7 +40,7 @@ describe("createEvents", () => {
   // })
 
   test("Deve criar um evento com sucesso", async () => {
-    const createEvent = new CreateEvent(new EventRepositoryDrizzle(database))
+    const { sut } = makeSut()
 
     const input = {
       name: "Geydson Event",
@@ -44,14 +51,14 @@ describe("createEvents", () => {
       ownerId: crypto.randomUUID(),
     }
 
-    const output = await createEvent.execute(input)
+    const output = await sut.execute(input)
     expect(output.id).toBeDefined()
     expect(output.name).toBe(input.name)
     expect(output.ticketPriceInCents).toBe(input.ticketPriceInCents)
   })
 
   test("Deve lançar um erro se o ownerId não for um UUID", async () => {
-    const createEvent = new CreateEvent(new EventRepositoryDrizzle(database))
+    const { sut } = makeSut()
 
     const input = {
       name: "Geydson Event",
@@ -62,12 +69,12 @@ describe("createEvents", () => {
       ownerId: "invalid-uuid",
     }
 
-    const output = createEvent.execute(input)
+    const output = sut.execute(input)
     await expect(output).rejects.toThrow(new Error("Invalid ownerId"))
   })
 
   test("Deve lançar um erro se o ticket price for negativo", async () => {
-    const createEvent = new CreateEvent(new EventRepositoryDrizzle(database))
+    const { sut } = makeSut()
 
     const input = {
       name: "Geydson Event",
@@ -78,12 +85,12 @@ describe("createEvents", () => {
       ownerId: crypto.randomUUID(),
     }
 
-    const output = createEvent.execute(input)
+    const output = sut.execute(input)
     await expect(output).rejects.toThrow(new Error("Invalid ticket price"))
   })
 
   test("Deve lançar um erro se a latitude for inválida", async () => {
-    const createEvent = new CreateEvent(new EventRepositoryDrizzle(database))
+    const { sut } = makeSut()
 
     const input = {
       name: "Geydson Event",
@@ -94,12 +101,12 @@ describe("createEvents", () => {
       ownerId: crypto.randomUUID(),
     }
 
-    const output = createEvent.execute(input)
+    const output = sut.execute(input)
     await expect(output).rejects.toThrow(new Error("Invalid latitude"))
   })
 
   test("Deve lançar um erro se a longitude for inválida", async () => {
-    const createEvent = new CreateEvent(new EventRepositoryDrizzle(database))
+    const { sut } = makeSut()
 
     const input = {
       name: "Geydson Event",
@@ -110,12 +117,12 @@ describe("createEvents", () => {
       ownerId: crypto.randomUUID(),
     }
 
-    const output = createEvent.execute(input)
+    const output = sut.execute(input)
     await expect(output).rejects.toThrow(new Error("Invalid longitude"))
   })
 
   test("Deve lançar um erro se a data for no passado", async () => {
-    const createEvent = new CreateEvent(new EventRepositoryDrizzle(database))
+    const { sut } = makeSut()
 
     const input = {
       name: "Geydson Event",
@@ -126,14 +133,14 @@ describe("createEvents", () => {
       ownerId: crypto.randomUUID(),
     }
 
-    const output = createEvent.execute(input)
+    const output = sut.execute(input)
     await expect(output).rejects.toThrow(
       new Error("Date must be in the future")
     )
   })
 
   test("Deve lançar um erro se já existir um evento com a mesma data, longitude e latitude", async () => {
-    const createEvent = new CreateEvent(new EventRepositoryDrizzle(database))
+    const { sut } = makeSut()
 
     const date = new Date(new Date().setHours(new Date().getHours() + 2))
     const input = {
@@ -145,11 +152,29 @@ describe("createEvents", () => {
       ownerId: crypto.randomUUID(),
     }
 
-    const output = await createEvent.execute(input)
+    const output = await sut.execute(input)
     expect(output.name).toBe(input.name)
     expect(output.ticketPriceInCents).toBe(input.ticketPriceInCents)
 
-    const output2 = createEvent.execute(input)
+    const output2 = sut.execute(input)
     await expect(output2).rejects.toThrow(new Error("Event already exists"))
+  })
+
+  test("Deve chamar o repository com os parâmetros corretos", async () => {
+    const { sut, eventRepository } = makeSut()
+    const spy = vi.spyOn(eventRepository, "create")
+
+    const input = {
+      name: "Geydson Event",
+      ticketPriceInCents: 5000,
+      latitude: 37.7749,
+      longitude: -122.4194,
+      date: new Date(new Date().setHours(new Date().getHours() + 1)),
+      ownerId: crypto.randomUUID(),
+    }
+
+    await sut.execute(input)
+
+    expect(spy).toHaveBeenCalledWith({ id: expect.any(String), ...input })
   })
 })
